1522fab211918fb9c4c04aeaaf14f35b49a7110d
bitten <1730648559@qq.com>
2024-06-21 14:44:23 +0800

    feat: completed 8.9.

---
+++ src/chapter8/8.9_update-children.spec.ts
@@ -0,0 +1,59 @@
+import { expect, test } from 'vitest'
+import { initRenderContainer } from '../utils/util'
+import { createRenderer } from './8.9_update-children'
+import { VNode } from '../utils/type'
+
+test('update-children-test', () => {
+    const appContainer = initRenderContainer()
+    const { render } = createRenderer()
+    const vnode_c: VNode = {
+        type: 'div',
+        props: {
+            id: 'foo',
+        },
+        children: [
+            {
+                type: 'div',
+                props: {
+                    id: 'child1',
+                },
+            },
+            {
+                type: 'div',
+                props: {
+                    id: 'child2',
+                },
+            },
+        ],
+    }
+    const vnode_e: VNode = {
+        type: 'div',
+        props: {
+            id: 'foo',
+        },
+    }
+    const vnode_t: VNode = {
+        type: 'div',
+        props: {
+            id: 'foo',
+        },
+        children: 'just text',
+    }
+
+    render(vnode_c, appContainer)
+    expect(document.querySelector('#child1')).not.toBeNull()
+    expect(document.querySelector('#child2')).not.toBeNull()
+    render(vnode_t, appContainer)
+    expect(document.querySelector('#child1')).toBeNull()
+    expect(document.querySelector('#child2')).toBeNull()
+    expect(document.querySelector('#foo')?.textContent).toEqual('just text')
+    render(vnode_e, appContainer)
+    expect(document.querySelector('#foo')?.children).toHaveLength(0)
+    render(vnode_t, appContainer)
+    expect(document.querySelector('#foo')?.textContent).toEqual('just text')
+    render(vnode_c, appContainer)
+    expect(document.querySelector('#foo')?.textContent).toEqual('')
+    expect(document.querySelector('#child1')).not.toBeNull()
+    expect(document.querySelector('#child2')).not.toBeNull()
+
+})
